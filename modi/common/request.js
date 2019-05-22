946b9b3cc40f823b06dbd1e1476ec440ed76c2e2
liyuepeng <wamk08@sina.com>
2019-05-22 17:53:07 +0800

    修改post请求头，修复post请求报错400的问题

--- modi/common/request.js
+++ modi/common/request.js
@@ -1,13 +1,22 @@
 import config from './config.js'
 
+/*
+	请求说明：
+	post请求需要添加响应头为  application/x-www-form-urlencoded 。将参数转换成 query string。  以 form-data 的形式传参
+	get请求，直接会将 参数 改成 query string
+*/
+
 // user-controller
 export async function checkLoginName(loginName) {
 	let result = uni.request({
 		url: config.host +'/m/user/checkLoginName',
 		method: 'POST',
-		data: JSON.stringify({
-			loginName
-		})
+		data: {
+			loginName: "abcdefghi"
+		},
+		header: {
+			"content-type": 'application/x-www-form-urlencoded'
+		}
 	})
 	console.log(result)
 	return result
