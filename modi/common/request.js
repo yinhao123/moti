036aa3aafb682bbae5361b9f449dd41691b61d93
liyuepeng <wamk08@sina.com>
2019-05-22 18:57:19 +0800

    添加完user-controller的接口

--- modi/common/request.js
+++ modi/common/request.js
@@ -1,7 +1,7 @@
 import config from './config.js'
 
 // 封装post方法
-async function post(url, data) {
+async function post (url, data) {
 	return await uni.request({
 		url: config.host + url,
 		method: 'POST',
@@ -13,10 +13,31 @@ async function post(url, data) {
 }
 
 // user-controller
-export function checkLoginName(loginName) {
-	let result = post('/m/user/checkLoginName', {
+export function checkLoginName (loginName) { // 参数：用户名
+	return post('/m/user/checkLoginName', {
 		loginName
 	})
-	return result
 }
+
+export function checkUserMobile (mobile) { // 参数：手机号
+	return post('/m/user/checkUserMobile', {
+		mobile
+	})
+}
+
+export function getDynamicCode (mobile) { // 参数：手机号
+	return post('/m/user/getDynamicCode', {
+		mobile
+	})
+}
+
+export function regist (loginName, password, mobile, dynamicCode) { // 参数：用户名，密码，手机号，验证码
+	return post('/m/user/regist', {
+		loginName,
+		password,
+		mobile,
+		dynamicCode
+	})
+}
+
 
