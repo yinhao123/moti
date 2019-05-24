a64e2f75aebadb75a874d11046587abc38ca3724
leewenlong6 <leewenlong6@gmail.com>
2019-05-24 15:59:56 +0800

    【rev】修改绑定手机页面样式调整完成

--- modi/pages/perfectInfo/phoneReset/phoneReset.vue
+++ modi/pages/perfectInfo/phoneReset/phoneReset.vue
@@ -1,6 +1,6 @@
 <template>
 	<view class="content">
-		<view class="hint">
+		<view class="hint-wrapper">
 			<text class="hint-text">当前绑定手机号</text>
 			<text class="hint-phone">188 **** 4435</text>
 		</view>
@@ -11,12 +11,12 @@
 				<text class="iconfont moti-round_close_fill_light"></text>
 			</view>
 			<view class="submit-item">
-				<text class="hint">身份证号</text>
+				<text class="hint iconfont moti-yanzhengma">&#xe647;</text>
 				<input type="text" name="code" placeholder="请输入验证码" v-model="code" />
 				<view class="getCode">获取验证码</view>
 			</view>
 		</view>
-		<view class="hint">修改手机后，可以使用新手机号登录MOTI到家</view>
+		<view class="hint-text">修改手机后，可以使用新手机号登录MOTI到家</view>
 		<view class="buttom-wrapper">
 			<view class="buttom">
 				确定更改手机号码
@@ -29,8 +29,8 @@
 	export default {
 		data() {
 			return {
-				phone:'',
-				code:''
+				phone: '',
+				code: ''
 			};
 		}
 	}
@@ -40,50 +40,91 @@
 	.content {
 		width: 100%;
 		height: 100%;
+		background: #f6f6f6;
 		display: flex;
 		flex-direction: column;
 		justify-content: space-between;
 
-		.hint,
-		.submit-item {
-			padding: 0 30upx;
-			display: flex;
-			align-items: center;
-			justify-content: space-between;
-			font-size: 22upx;
-		}
-
-		.hint {
-			height: 82upx;
-			line-height: 82upx;
+		.hint-text {
 			font-family: MicrosoftYaHei;
+			font-size: 22upx;
 			font-weight: normal;
 			font-stretch: normal;
-			letter-spacing: 0px;
-			color: #666666;
+			color: #999999;
+		}
+
+		.hint-wrapper {
+			width: 100%;
+			height: 165upx;
+			padding-top: 120upx;
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+
+			text {
+				text-align: center;
+			}
+
+			.hint-text {
+				margin-bottom: 40upx;
+			}
+
+			.hint-phone {
+				font-family: MicrosoftYaHei;
+				font-size: 30upx;
+				font-weight: normal;
+				font-stretch: normal;
+				color: #000000;
+			}
 		}
 
 		.submit-list {
 			background: #ffffff;
 
 			.submit-item {
+				height: 98upx;
+				padding-right: 30upx;
+				display: flex;
+				justify-content: space-between;
+				align-items: center;
 				border-bottom: 2upx solid #f6f6f6;
 
 				.hint {
-					padding-left: 0;
-					color: #000000;
+					width: 120upx;
+					text-align: center;
+					font-size: 24upx;
 				}
 
 				input {
-					flex: 1
+					flex: 1;
 				}
 
 				.iconfont {
-					padding: 5upx;
-					color: #3e3e3e;
-					opacity: 0.4;
+					color: #999999;
+					font-size: 34upx;
+				}
+
+				.moti-yanzhengma {
+					font-size: 48upx;
+				}
+
+				.getCode {
+					width: 180upx;
+					height: 60upx;
+					background-color: #fa5151;
+					border-radius: 8upx;
+					text-align: center;
+					line-height: 60upx;
+					color: #ffffff;
+					font-family: MicrosoftYaHei;
+					font-size: 22upx;
 				}
 			}
+		}
+
+		>.hint-text {
+			padding-top: 80upx;
+			text-align: center;
 		}
 
 		.buttom-wrapper {
